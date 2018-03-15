4c228dc58486b0d044d7e6d794df4333136f05a4
daoge1205 <870818962@qq.com>
2018-03-15 14:36:31 +0800

    20180315

---
+++ host.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="bootstrap.min.css">
+<script src="jquery.min.js"></script>
+<script src="popper.min.js"></script>
+<script src="bootstrap.min.js"></script>
+
+<title>毕业设计首页</title>
+<style>
+	#d1{
+		background-color:grey;
+		height:50px;
+		float:left;
+		width:100%;
+		text-align:center;
+	}
+	#d21{
+		background-color:black;
+		color:white;
+		float:left;
+		width:25%;
+		text-align:center;
+	}
+	 #d22{
+                background-color:black;
+                color:white;
+                float:left;
+                width:25%;
+                text-align:center;
+        }
+	 #d23{
+                background-color:black;
+                color:white;
+                float:left;
+                width:25%;
+                text-align:center;
+        }
+	 #d24{
+                background-color:black;
+                color:white;
+                float:left;
+                width:25%;
+                text-align:center;
+        }
+	#lbq{
+		width:100%;
+		height:445px;
+		float:left;
+	}
+	.carousel-inner img {
+     		 width: 100%;
+    		 height: 100%;
+  	}
+
+
+
+</style>
+</head>
+
+<body>
+	<div id="d1">
+		这是广告栏目
+	</div>
+	<div id="d21">
+		图书查询
+	</div>
+	 <div id="d22">
+                好书推荐
+        </div>
+	 <div id="d23">
+                用户登陆
+        </div>
+	 <div id="d24">
+                管理者选项
+        </div>
+	<div id="lbq">
+		<div id="demo" class="carousel slide" data-ride="carousel">
+ 		 <!-- 指示符 -->
+		  <ul class="carousel-indicators">
+		    <li data-target="#demo" data-slide-to="0" class="active"></li>
+		    <li data-target="#demo" data-slide-to="1"></li>
+		    <li data-target="#demo" data-slide-to="2"></li>
+		  </ul>
+		  <!-- 轮播图片 -->
+ 		 <div class="carousel-inner">
+  			  <div class="carousel-item active">
+  			   	 <img src="img_fjords_wide.jpg">
+			  </div>
+  		 	  <div class="carousel-item">
+   		  	   	 <img src="img_nature_wide.jpg">
+   			  </div>
+   			 <div class="carousel-item">
+     		 		<img src="img_mountains_wide.jpg">
+  			  </div>
+		  </div>
+ 
+		  <!-- 左右切换按钮 -->
+			 <a class="carousel-control-prev" href="#demo" data-slide="prev">
+   				 <span class="carousel-control-prev-icon"></span>
+ 		   	 </a>
+			 <a class="carousel-control-next" href="#demo" data-slide="next">
+			   	 <span class="carousel-control-next-icon"></span>
+			 </a>
+
+		</div>
+		
+	</div>
+
+
+
+
+</body>
+</html>
